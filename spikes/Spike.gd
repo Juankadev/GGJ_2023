e5c04f8e54415df581b2c3884a3dbca2ca1be3f2
Gian <catania.gian@gmail.com>
2023-02-04 19:12:14 -0300

    removed timer until further notice

--- spikes/Spike.gd
+++ spikes/Spike.gd
@@ -2,19 +2,21 @@ extends Area2D
 
 var player: Node = null
 
+# TODO(beto): make timer work
 func _on_Spike_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
-		player = body
-		$"../../Player".on_killed()
-		$"../../Timer".start()
-		
+#		player = body
+		body.position = $"../../Position2D".position
+#		$"../../Player".on_killed()
+#		$"../../Timer".start()
+
 	if body.is_in_group("box"):
 		body.position = Vector2.ZERO
-
-func _on_Timer_timeout() -> void:
-	print("timer timed out")
-	if player:
-		$"../../Player".on_respawn()
-		player.position = $"../../Position2D".position
-
-	$"../../Timer".stop()
+#
+#func _on_Timer_timeout() -> void:
+#	print("timer timed out")
+#	if player:
+#		$"../../Player".on_respawn()
+#		player.position = $"../../Position2D".position
+#
+#	$"../../Timer".stop()
