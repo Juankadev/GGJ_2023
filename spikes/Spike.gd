f640ad11b30251aa47ee2e2904f73bfd469628d1
Gian <catania.gian@gmail.com>
2023-02-04 18:23:41 -0300

    ParentBox

--- spikes/Spike.gd
+++ spikes/Spike.gd
@@ -3,3 +3,6 @@ extends Area2D
 func _on_Spike_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
 		body.position = $"../../Position2D".position
+		
+	if body.is_in_group("box"):
+		body.position = Vector2.ZERO
