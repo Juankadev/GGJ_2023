1cfd5afaad9b215e4f8f6d6951001b20ec365100
Juan Cruz Rey <54191222+Juankadev@users.noreply.github.com>
2023-02-05 01:26:23 -0300

    Collect Gem

--- Gema.gd
+++ Gema.gd
@@ -2,12 +2,16 @@ extends Area2D
 
 var agarrado=false
 var player
+onready var gema
 
 func _physics_process(delta):
+	gema = get_parent().get_parent().get_node("Player/posGema").global_position
 	if agarrado:
-		self.position=player.position
+		#var newpos = Vector2(player.position.x + 20, player.position.y - 40)
+		self.position=gema
 
 func _on_Gema_body_entered(body):
 	if body.get_name()=="Player":
 		agarrado=true
 		player=body
+		
